78f23d259a8251c4dd7ad84cd0e621611328e8b5
YonYoon <zhzhzh1806@gmail.com>
2024-09-27 09:50:05 +0500

    Create new file 'StudyView'

---
+++ StudyOn/StudyView.swift
@@ -0,0 +1,18 @@
+//
+//  StudyView.swift
+//  StudyOn
+//
+//  Created by Zhansen Zhalel on 27.09.2024.
+//
+
+import SwiftUI
+
+struct StudyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudyView()
+}
